09b494432c3229ecba9ddb5e48a9a1d4ac0121cb
Dick Olsson <dick.olsson@senzilla.com>
2014-05-31 23:25:58 -0500

    Removed unused variable

--- src/Plugin/Field/FieldType/LocalSequenceItemList.php
+++ src/Plugin/Field/FieldType/LocalSequenceItemList.php
@@ -7,7 +7,7 @@ use Drupal\Core\Field\FieldItemList;
 class LocalSequenceItemList extends FieldItemList {
 
   public function preSave() {
-    $item = $this->get(0)->id = (string) microtime(TRUE);
+    $this->get(0)->id = (string) microtime(TRUE);
     parent::preSave();
   }
 }
