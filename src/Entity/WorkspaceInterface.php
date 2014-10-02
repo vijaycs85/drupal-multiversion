bb61e79d6cf64c1ed935853945cd6125a96a0057
Dick Olsson <dick.olsson@senzilla.com>
2014-10-02 14:46:48 +0200

    Cleaned up the implementation of the Workspace config entity.

--- src/Entity/WorkspaceInterface.php
+++ src/Entity/WorkspaceInterface.php
@@ -6,12 +6,4 @@ use Drupal\Core\Config\Entity\ConfigEntityInterface;
 
 interface WorkspaceInterface extends ConfigEntityInterface {
 
-  /**
-   * Returns the workspace name.
-   *
-   * @return string
-   *   The name of the workspace.
-   */
-  public function name();
-
 }
