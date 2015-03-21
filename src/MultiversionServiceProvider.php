f033ef7b183a61c248631fa82648ce24d2b33b26
Dick Olsson <dick.olsson@senzilla.com>
2015-03-21 13:56:15 +0000

    Do not use the module handler when altering the container.

--- src/MultiversionServiceProvider.php
+++ src/MultiversionServiceProvider.php
@@ -9,6 +9,7 @@ namespace Drupal\multiversion;
 
 use Drupal\Core\DependencyInjection\ContainerBuilder;
 use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use \Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
 
 /**
  * Defines a service profiler for the multiversion module.
@@ -17,9 +18,12 @@ class MultiversionServiceProvider extends ServiceProviderBase {
 
   public function alter(ContainerBuilder $container) {
     // Override the comment.statistics class with a new class.
-    if (\Drupal::moduleHandler()->moduleExists('comment')) {
+    try {
       $definition = $container->getDefinition('comment.statistics');
       $definition->setClass('Drupal\multiversion\CommentStatistics');
+    }
+    catch (InvalidArgumentException $e) {
+      // Do nothing, comment module is not installed.
     }
   }
 
