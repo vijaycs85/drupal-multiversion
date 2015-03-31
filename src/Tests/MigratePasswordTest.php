681651af927145585b00ee32ceab6e25c497f988
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-03-31 15:57:43 +0300

    Test password hashing on users migration.

---
+++ src/Tests/MigratePasswordTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\multiversion\Tests\MigratePasswordTest.
+ */
+
+namespace Drupal\multiversion\Tests;
+
+/**
+ * Test password hashing on migration.
+ *
+ * @group multiversion
+ */
+class MigratePasswordTest extends MultiversionWebTestBase {
+
+  public function testPasswordMigration() {
+    $password_migrate = \Drupal::service('password_migrate');
+    $password = $this->randomMachineName();
+    $migrated_password = $password_migrate->hash($password);
+    $this->assertNotEqual($migrated_password, $password, 'Migrated password was hashed.');
+
+    $password_migrate->disablePasswordHashing();
+    $password = $this->randomMachineName();
+    $migrated_password = $password_migrate->hash($password);
+    $this->assertEqual($migrated_password, $password, 'Migrated password was not hashed.');
+  }
+}
