1b2ea5923d29cd1ae2f32d8801d621bfef48ecdb
Andrei Jechiu <andrei.jechiu@gmail.com>
2015-09-09 18:34:31 +0300

    Add tests for workspace filter.

--- src/Tests/Views/WorkspaceTest.php
+++ src/Tests/Views/WorkspaceTest.php
@@ -2,34 +2,32 @@
 
 /**
  * @file
- * Contains \Drupal\multiversion\Tests\Views\CurrentWorkspaceTest.
+ * Contains \Drupal\multiversion\Tests\Views\WorkspaceTest.
  */
 
 namespace Drupal\multiversion\Tests\Views;
 
 /**
- * Tests the current_workspace field handler.
+ * Tests the workspace and current_workspace field handlers.
  *
  * @group multiversion
  * @see \Drupal\multiversion\Plugin\views\filter\CurrentWorkspace
  */
-class CurrentWorkspaceTest extends MultiversionTestBase {
+class WorkspaceTest extends MultiversionTestBase {
 
   protected $strictConfigSchema = FALSE;
-
-  //protected $profile = 'standard';
 
   /**
    * Views used by this test.
    *
    * @var array
    */
-  public static $testViews = array('test_current_workspace');
+  public static $testViews = array('test_workspace', 'test_current_workspace');
 
   /**
-   * Tests the current workspace filter.
+   * Tests the workspace filter.
    */
-  public function testCurrentWorkspace() {
+  public function testWorkspace() {
     $admin_user = $this->drupalCreateUser(array('bypass node access'));
     $uid = $admin_user->id();
     $this->drupalLogin($admin_user);
@@ -47,6 +45,20 @@ class CurrentWorkspaceTest extends MultiversionTestBase {
     $node3 = $this->drupalCreateNode(array('uid' => $uid));
     $node4 = $this->drupalCreateNode(array('uid' => $uid));
 
+    // Test workspace filter.
+    $this->drupalGet('test_workspace', ['query' => ['workspace' => 'new_workspace']]);
+    $this->assertNoText($node1->label());
+    $this->assertNoText($node2->label());
+    $this->assertNoText($node3->label());
+    $this->assertNoText($node4->label());
+
+    $this->drupalGet('test_workspace', ['query' => ['workspace' => 'default']]);
+    $this->assertText($node1->label());
+    $this->assertText($node2->label());
+    $this->assertNoText($node3->label());
+    $this->assertNoText($node4->label());
+
+    // Test current_workspace filter.
     $this->drupalGet('test_current_workspace', ['query' => ['workspace' => 'new_workspace']]);
     $this->assertNoText($node1->label());
     $this->assertNoText($node2->label());
