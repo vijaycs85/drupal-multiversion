cfbfaf3a3747e51ef9ea7155af20196fa3507ecd
dickolsson <dick.olsson@senzilla.com>
2015-10-26 20:41:55 +0000

    Made the EntityQueryTest less rigid now that revision IDs behave differently after implementing the new migration setup.

--- src/Tests/EntityQueryTest.php
+++ src/Tests/EntityQueryTest.php
@@ -65,16 +65,16 @@ class EntityQueryTest extends MultiversionWebTestBase {
       $entity->save();
 
       // For user entity type we expect to have three entities: anonymous, root
-      // user and the new created entity (anonymous - 0, admin - 1, new user - 3).
-      $expected_results = $entity_type_id == 'user' ? array(1 => '0', 2 => '1', 3 => '3') : array(1 => '1');
+      // user and the new created entity (anonymous - 0, admin - 1, new user - 2).
+      $expected_results = $entity_type_id == 'user' ? array('0', '1', '2') : array('1');
       $results = $this->factory->get($entity_type_id)
         ->execute();
-      $this->assertIdentical($results, $expected_results, "Query without isNotDeleted on existing $entity_type_id returned expected result.");
+      $this->assertIdentical(array_values($results), $expected_results, "Query without isNotDeleted on existing $entity_type_id returned expected result.");
 
       $results = $this->factory->get($entity_type_id)
         ->isNotDeleted()
         ->execute();
-      $this->assertIdentical($results, $expected_results, "Query with isNotDeleted on existing $entity_type_id returned expected result.");
+      $this->assertIdentical(array_values($results), $expected_results, "Query with isNotDeleted on existing $entity_type_id returned expected result.");
 
       $results = $this->factory->get($entity_type_id)
         ->isDeleted()
@@ -84,11 +84,10 @@ class EntityQueryTest extends MultiversionWebTestBase {
       // For user entity type we have three entities: anonymous, root user and
       // the new created user.
       $revision = $entity_type_id == 'user' ? 3 : 1;
-      $expected_results = $entity_type_id == 'user' ? array(3 => '3') : array(1 => '1');
       $results = $this->factory->get($entity_type_id)
         ->condition($entity_type->getKey('revision'), $revision)
         ->execute();
-      $this->assertIdentical($results, $expected_results, "Revision query on existing $entity_type_id returned expected result.");
+      $this->assertIdentical(count($results), 1, "Revision query on existing $entity_type_id returned expected result.");
 
       // Now delete the entity.
       $entity->delete();
@@ -96,29 +95,26 @@ class EntityQueryTest extends MultiversionWebTestBase {
       // For user entity type we expect to have two entities: anonymous and
       // admin (anonymous - 0, admin - 1). Deleted user's id shouldn't be in the
       // results array.
-      $expected_results = $entity_type_id == 'user' ? array(1 => '0', 2 => '1') : array();
+      $expected_results = $entity_type_id == 'user' ? array('0', '1') : array();
       $results = $this->factory->get($entity_type_id)
         ->execute();
-      $this->assertIdentical($results, $expected_results, "Query without isNotDeleted on deleted $entity_type_id returned expected result.");
+      $this->assertIdentical(array_values($results), $expected_results, "Query without isNotDeleted on deleted $entity_type_id returned expected result.");
 
       $results = $this->factory->get($entity_type_id)
         ->isNotDeleted()
         ->execute();
-      $this->assertIdentical($results, $expected_results, "Query with isNotDeleted on deleted $entity_type_id returned expected result.");
+      $this->assertIdentical(array_values($results), $expected_results, "Query with isNotDeleted on deleted $entity_type_id returned expected result.");
 
-      // We expect array(4 => '3') for user entity type: 4 is the revision ID
-      // and 3 the entity ID. Revision ID is 4 because we had three users
-      // before deletion of the user with id 3.
-      $expected_results = $entity_type_id == 'user' ? array(4 => '3') : array(2 => '1');
+      $expected_results = $entity_type_id == 'user' ? array('2') : array('1');
       $results = $this->factory->get($entity_type_id)
         ->isDeleted()
         ->execute();
-      $this->assertIdentical($results, $expected_results, "Query with isDeleted on deleted $entity_type_id returned expected result.");
+      $this->assertIdentical(array_values($results), $expected_results, "Query with isDeleted on deleted $entity_type_id returned expected result.");
 
       $results = $this->factory->get($entity_type_id)
         ->condition($entity_type->getKey('revision'), 2)
         ->execute();
-      $this->assertIdentical($results, array(2 => '1'), "Revision query on deleted $entity_type_id returned expected result.");
+      $this->assertIdentical(count($results), 1, "Revision query on deleted $entity_type_id returned expected result.");
     }
   }
 
