7719eceb3537ff23bfe0f391d0132fb33327e441
Tim Millwood <tim@millwoodonline.co.uk>
2016-01-29 15:35:34 +0000

    Preventing workspaces types with workspaces from being deleted

--- src/Form/WorkspaceTypeDeleteForm.php
+++ src/Form/WorkspaceTypeDeleteForm.php
@@ -7,51 +7,55 @@
 
 namespace Drupal\multiversion\Form;
 
-use Drupal\Core\Entity\EntityConfirmFormBase;
+use Drupal\Core\Entity\EntityDeleteForm;
+use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Builds the form to delete Workspace type entities.
+ * Provides a confirmation form for deleting a custom workspace type entity.
  */
-class WorkspaceTypeDeleteForm extends EntityConfirmFormBase {
+class WorkspaceTypeDeleteForm extends EntityDeleteForm {
+
   /**
-   * {@inheritdoc}
+   * The query factory to create entity queries.
+   *
+   * @var \Drupal\Core\Entity\Query\QueryFactory
    */
-  public function getQuestion() {
-    return $this->t('Are you sure you want to delete %name?', array('%name' => $this->entity->label()));
+  public $queryFactory;
+
+  /**
+   * Constructs a query factory object.
+   *
+   * @param \Drupal\Core\Entity\Query\QueryFactory $query_factory
+   *   The entity query object.
+   */
+  public function __construct(QueryFactory $query_factory) {
+    $this->queryFactory = $query_factory;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getCancelUrl() {
-    return new Url('entity.workspace_type.collection');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getConfirmText() {
-    return $this->t('Delete');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->entity->delete();
-
-    drupal_set_message(
-      $this->t('content @type: deleted @label.',
-        [
-          '@type' => $this->entity->bundle(),
-          '@label' => $this->entity->label()
-        ]
-        )
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity.query')
     );
+  }
 
-    $form_state->setRedirectUrl($this->getCancelUrl());
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $workspaces = $this->queryFactory->get('workspace')->condition('type', $this->entity->id())->execute();
+    if (!empty($workspaces)) {
+      $caption = '<p>' . $this->formatPlural(count($workspaces), '%label is used by 1 workspace on your site. You can not remove this workspace type until you have removed all of the %label workspaces.', '%label is used by @count workspaces on your site. You may not remove %label until you have removed all of the %label custom workspaces.', array('%label' => $this->entity->label())) . '</p>';
+      $form['description'] = array('#markup' => $caption);
+      return $form;
+    }
+    else {
+      return parent::buildForm($form, $form_state);
+    }
   }
 
 }
