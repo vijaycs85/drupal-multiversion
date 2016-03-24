25bc4626456e41da24d9da7d48f891bbeb77fd11
Crell <Crell@26398.no-reply.drupal.org>
2016-03-24 13:34:00 +0000

    Issue #2692527 by Crell: Don't allow switching to an inaccessible workspace

--- src/Workspace/WorkspaceManager.php
+++ src/Workspace/WorkspaceManager.php
@@ -8,10 +8,14 @@
 namespace Drupal\multiversion\Workspace;
 
 use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\multiversion\Entity\WorkspaceInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class WorkspaceManager implements WorkspaceManagerInterface {
+  use StringTranslationTrait;
 
   /**
    * @var \Symfony\Component\HttpFoundation\RequestStack
@@ -40,12 +44,22 @@ class WorkspaceManager implements WorkspaceManagerInterface {
   protected $activeWorkspace;
 
   /**
-   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
-   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   * @var \Psr\Log\LoggerInterface
    */
-  public function __construct(RequestStack $request_stack, EntityManagerInterface $entity_manager) {
+  protected $logger;
+
+  /**
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack.
+   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
+   *   The entity manager. This should get replaced with Entity Type Manager.
+   * @param LoggerInterface $logger
+   *   A logger service.
+   */
+  public function __construct(RequestStack $request_stack, EntityManagerInterface $entity_manager, LoggerInterface $logger = NULL) {
     $this->requestStack = $request_stack;
     $this->entityManager = $entity_manager;
+    $this->logger = $logger ?: new NullLogger();
   }
 
   /**
@@ -84,31 +98,53 @@ class WorkspaceManager implements WorkspaceManagerInterface {
    * @todo {@link https://www.drupal.org/node/2600382 Access check.}
    */
   public function getActiveWorkspace() {
-    // Return the cached value if it is set.
-    if (isset($this->activeWorkspace)) {
-      return $this->activeWorkspace;
-    }
 
-    $this->activeWorkspace = NULL;
-    $request = $this->requestStack->getCurrentRequest();
-    foreach ($this->getSortedNegotiators() as $negotiator) {
-      if ($negotiator->applies($request)) {
-        if ($workspace_id = $negotiator->getWorkspaceId($request)) {
-          if ($active_workspace = $this->load($workspace_id)) {
-            $this->activeWorkspace = $active_workspace;
-            break;
-          }
-        }
-      }
+    if (!$this->activeWorkspace) {
+      $this->activeWorkspace = $this->deriveActiveWorkspace();
     }
 
     return $this->activeWorkspace;
   }
 
   /**
+   * Determines the active workspace from the available negotiators.
+   *
+   * This method is uncached, and intended to be used by a caching public
+   * method.
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|null
+   *   The workspace that should be active, or NULL if there isn't one.
+   */
+  protected function deriveActiveWorkspace() {
+    $active_workspace = NULL;
+    $request = $this->requestStack->getCurrentRequest();
+    foreach ($this->getSortedNegotiators() as $negotiator) {
+      if ($negotiator->applies($request)) {
+        if ($workspace_id = $negotiator->getWorkspaceId($request)) {
+          if ($new_active_workspace = $this->load($workspace_id)) {
+            $active_workspace = $new_active_workspace;
+            break;
+          }
+        }
+      }
+    }
+    return $active_workspace;
+
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function setActiveWorkspace(WorkspaceInterface $workspace) {
+
+    // If the current user doesn't have access to view the workspace, they
+    // shouldn't be allowed to switch to it.
+    // @todo Could this be handled better?
+    if (!$workspace->access('view')) {
+      $this->logger->error('Denied access to view workspace {workspace}', ['workspace' => $workspace->label()]);
+      throw new WorkspaceAccessException('The user does not have permission to view that workspace.');
+    }
+
     // Set the workspace on the proper negotiator.
     $request = $this->requestStack->getCurrentRequest();
     foreach ($this->getSortedNegotiators() as $negotiator) {
