b32b43bd995a05e7855fb4acf0b558c1ceb04c69
Andrei Jechiu <andrei.jechiu@gmail.com>
2016-04-06 12:20:00 +0300

    Change the plugin id, this will make possible to have a new entry in the menu_tree table for menu links with the same UUID.

---
+++ src/Entity/MenuLinkContent.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\multiversion\Entity\MenuLinkContent.
+ */
+
+namespace Drupal\multiversion\Entity;
+
+use Drupal\menu_link_content\Entity\MenuLinkContent as CoreMenuLinkContent;
+
+class MenuLinkContent extends CoreMenuLinkContent {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluginId() {
+    return 'menu_link_content:' . $this->uuid() . ':' . $this->getRevisionId();
+  }
+
+}
