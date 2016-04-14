cb294265684f1567b108f05dd4731c63a6dfe313
Andrei Jechiu <andrei.jechiu@gmail.com>
2016-04-14 11:40:27 +0300

    Fix warning on menu link content replication.

--- src/Entity/MenuLinkContent.php
+++ src/Entity/MenuLinkContent.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\multiversion\Entity;
 
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\menu_link_content\Entity\MenuLinkContent as CoreMenuLinkContent;
 
 class MenuLinkContent extends CoreMenuLinkContent {
@@ -15,7 +16,21 @@ class MenuLinkContent extends CoreMenuLinkContent {
    * {@inheritdoc}
    */
   public function getPluginId() {
+    // Make the plugin ID unique adding the entity ID.
     return 'menu_link_content:' . $this->uuid() . ':' . $this->id();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function preSave(EntityStorageInterface $storage) {
+    // If the $this->link->uri contains the entity object, presave it and
+    // replace the entity object with the uri.
+    // @see \Drupal\multiversion\LinkItem::preSave().
+    if (is_object($this->link->uri)) {
+      $this->link->preSave();
+    }
+    parent::preSave($storage);
   }
 
 }
