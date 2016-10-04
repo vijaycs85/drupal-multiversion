271bacd54b487426de58ec7c5bf088f79b8cfb65
jkenn99 <jkenn99@561498.no-reply.drupal.org>
2016-10-04 06:51:25 +0300

    Issue #2778683 by JamesK, jeqq: Alter preSave() method for file and image field item classes.

--- src/FileItem.php
+++ src/FileItem.php
@@ -2,14 +2,14 @@
 
 namespace Drupal\multiversion;
 
-use \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem as CoreEntityReferenceItem;
+use \Drupal\file\Plugin\Field\FieldType\FileItem as CoreFileItem;
 
 /**
- * Alternative entity reference base field item type class.
+ * Alternative file field item type class.
  *
  * @todo We have integrations tests that ensure this is working. But some unit
  *   tests would be good to ensure all possible scenarios are covered.
  */
-class EntityReferenceItem extends CoreEntityReferenceItem {
+class FileItem extends CoreFileItem {
   use EntityReferenceFieldTrait;
 }
--- src/ImageItem.php
+++ src/ImageItem.php
@@ -2,14 +2,14 @@
 
 namespace Drupal\multiversion;
 
-use \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem as CoreEntityReferenceItem;
+use \Drupal\image\Plugin\Field\FieldType\ImageItem as CoreImageItem;
 
 /**
- * Alternative entity reference base field item type class.
+ * Alternative image field item type class.
  *
  * @todo We have integrations tests that ensure this is working. But some unit
  *   tests would be good to ensure all possible scenarios are covered.
  */
-class EntityReferenceItem extends CoreEntityReferenceItem {
+class ImageItem extends CoreImageItem {
   use EntityReferenceFieldTrait;
 }
