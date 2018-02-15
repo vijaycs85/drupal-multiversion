9a39adf884305171ea9046a6019d1e3375a808b1
Andrei Jechiu <andrei.jechiu@gmail.com>
2018-02-15 15:16:51 +0200

    Decorate services instear of alter.

--- src/MultiversionServiceProvider.php
+++ src/MultiversionServiceProvider.php
@@ -22,27 +22,6 @@ class MultiversionServiceProvider extends ServiceProviderBase {
     $definition = $container->getDefinition('menu.tree_storage');
     $definition->setClass('Drupal\multiversion\MenuTreeStorage');
 
-    // Override the path.alias_storage class with a new class.
-    $definition = $container->getDefinition('path.alias_storage');
-    $definition->setClass('Drupal\multiversion\AliasStorage')
-      ->addArgument(new Reference('workspace.manager'))
-      ->addArgument(new Reference('entity_type.manager'))
-      ->addArgument(new Reference('state'));
-
-    // Override the router.route_provider class with a new class.
-    $definition = $container->getDefinition('router.route_provider');
-    $definition->setClass('Drupal\multiversion\RouteProvider')
-      ->setArguments([
-        new Reference('database'),
-        new Reference('state'),
-        new Reference('path.current'),
-        new Reference('cache.data'),
-        new Reference('path_processor_manager'),
-        new Reference('cache_tags.invalidator'),
-        'router',
-        new Reference('workspace.manager'),
-      ]);
-
     // Override the comment.statistics class with a new class.
     try {
       $definition = $container->getDefinition('comment.statistics');
