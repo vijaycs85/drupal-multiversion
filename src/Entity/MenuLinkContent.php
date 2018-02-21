8294e16e6632752c5ecf6c5f2d6cd324e8219fea
Andrei Jechiu <andrei.jechiu@gmail.com>
2018-02-21 12:15:35 +0200

    Support redirect entity type.

--- src/Entity/MenuLinkContent.php
+++ src/Entity/MenuLinkContent.php
@@ -2,7 +2,6 @@
 
 namespace Drupal\multiversion\Entity;
 
-use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\menu_link_content\Entity\MenuLinkContent as CoreMenuLinkContent;
 
 class MenuLinkContent extends CoreMenuLinkContent {
